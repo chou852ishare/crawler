36b1d3a1decc311b30ff0c6c6e28ae8c64b99c58
zhouzy <zhouzy.ustc@gmail.com>
2015-11-07 13:23:51 +0800

    add xiaoqu list crawler

--- house_price/crawler.py
+++ house_price/crawler.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
+
 import re
 import urllib2
+from bs4 import BeautifulSoup
 
 def format_citylist(infile, outfile):
     fi = open(infile, 'r')
@@ -20,14 +23,57 @@ def format_citylist(infile, outfile):
 
 
 def get_xiaoqulist(infile, outfile):
-    fi = open(infile, 'r')
-    fo = open(outfile, 'w')
-    print >> fo, 'xiaoqu_en,xiaoqu_cn,city_abbrev,city,province'
+    fi  = open(infile, 'r')
+    log = open('xiaoqu.log', 'w')
     lines = fi.readlines()
-    for line in lines:
-        abbr = line.split(',')[0]
-        alph = 'A'
-        print 'esf.%s.fang.com/housing/xiaoqu_%s.htm' % (abbr, alph)
+    for line in lines[1:]:
+        abbr = line.split(',')[0].encode('utf8')
+        city = line.split(',')[1].encode('utf8')
+        fo   = open(outfile+'_'+abbr, 'w')
+        print >> fo, 'xiaoqu_en,xiaoqu_cn,city_abbrev,city,province'
+        alph = 'ABCDEFG' + \
+               'HIJKLMN' + \
+               'OPQRST' + \
+               'UVWXYZ'
+        for x in alph:
+            try:
+                src  = urllib2.urlopen('http://esf.%s.fang.com/housing/xiaoqu_%s.htm' % (abbr, x), timeout = 5)
+                page = src.read().decode('gbk')
+                soup = BeautifulSoup(page)
+                guide = soup(class_='guide')[0].encode('utf8')
+                if city not in guide:
+                    continue
+                xiaoqu = soup(class_='busHsIndex')[0].find_all('a')
+                for xq in xiaoqu:
+                    vill = xq.text.encode('utf8').strip()
+                    href = xq['href']
+                    try:
+                        site = get_site(abbr, vill, href) 
+                        if site == '':
+                            print >> log, '%s,%s,%s,%s,%s,%s' % (abbr, city, x, vill, href, 'null_xiaoqu')
+                        else:
+                            print >> fo, vill + ',' + site
+                    except:
+                        print >> log, '%s,%s,%s,%s,%s,%s' % (abbr, city, x, vill, href, 'fail_xiaoqu')
+            except:
+                print >> log, '%s,%s,%s,%s' % (abbr, city, x, 'null')
+        fo.close()
+    fi.close()
+    log.close()
+
+
+def get_site(abbr, vill, href):
+    src1 = urllib2.urlopen('http://esf.%s.fang.com%s' % (abbr, href), timeout=5)
+    page = src1.read()
+    soup = BeautifulSoup(page)
+    hous = soup(class_='village mt10')[0]
+    vila = hous(class_='hTitle')[0].a.text.encode('utf8')
+    site = ''
+    if vill == vila:
+        site = hous(class_='iconXQ ml10')[0]['href']
+    return site
+    
+
 
 if __name__ == '__main__':
     cityhtml   = 'citylist_html'
