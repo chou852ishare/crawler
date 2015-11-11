1046c1cb56a9f6670e27e87da71bdb7d1f4b6f94
chou852ishare <chouzyz@gmail.com>
2015-11-11 22:50:29 +0800

    test baidu crawler

--- multi_store_game/baidu/crawler_baidugame.py
+++ multi_store_game/baidu/crawler_baidugame.py
@@ -9,10 +9,33 @@ def get_all_rank():
     print >> f, page
     f.close()
 
-def get_xiuxianyizhi_rank():
-    for i in range(1,9):
-        site = 'http://shouji.baidu.com/game/list?cid=401&page_num=%s' % i
+def get_category_rank():
+    pref = 'http://shouji.baidu.com/game/list?cid=%s&page_num=%s' 
+    cate = {1: 'xiuxianyizhi',
+            3: 'dongzuosheji',
+            5: 'tiyujingji',
+            8: 'jingyingyangcheng',
+            2: 'juesebanyan',
+            6: 'saichejingsu',
+            4: 'monifuzhu',
+            7: 'qipaizhuoyou'}
+    log = open('baidu.log', 'w')
+    for cid in cate.keys():
+        c = cate[cid]
+        for p in range(1,9):
+            f = open('%s_%s.html' % (c,p), 'w')
+            url = pref % (i, p)
+            try:
+                src  = urllib2.urlopen(url, timeout=5)
+                page = src.read()
+                print >> f, page
+                f.close()
+            except:
+                print >> log, c, p, 'fail'
+                continue
+    log.close()
+
 
 if __name__ == '__main__':
     get_all_rank()
-    get_xiuxianyizhi_rank()
+    get_category_rank()
