a6f980742e69652fc1fa0b3dc5cb122b670ae277
zhouzy <zhouzy.ustc@gmail.com>
2015-11-28 13:29:45 +0800

    add crawler

---
+++ lenovo_phone/crawler.py
@@ -0,0 +1,37 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib2
+import traceback
+from bs4 import BeautifulSoup
+
+prefix = 'http://product.cnmo.com/pro_sub_manu/sub_57_manu_1763_1_%s.shtml'
+output = './output/'
+
+def get_soup(url):
+    src  = urllib2.urlopen(url, timeout=3)
+    page = src.read()
+    soup = BeautifulSoup(page)
+    return soup
+
+
+def crawl_page(page):
+    url = prefix % page
+    try:
+        soup = get_soup(url)
+        itms = soup(class_='btn', id='comparePicList')
+        pars = [item('a')[1]['href'] for item in itms]
+        for purl in pars:
+            psoup = get_soup(purl)
+            fname = (output+'lenovo_').encode('u8') + psoup(class_='clearfix modle_title')[0]('h1')[0].text.encode('u8')
+            print >> open(fname, 'w'), psoup
+    except Exception, e:
+        print traceback.print_exc()
+
+
+def main():
+    for page in range(1,15):
+        crawl_page(page)
+
+if __name__ == '__main__':
+    main()
